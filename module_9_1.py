29081e375565ba423cf9b65d83561b8166f60559
OlegBurov88 <ovburov88@gmail.com>
2024-11-23 11:12:50 +0500

    Домашнее задание по теме "Введение в функциональное программирование"

---
+++ module_9_1.py
@@ -0,0 +1,10 @@
+# создаём функцию, для передачи и вызова функций как объекта.
+def apply_all_func(int_list, *functions):
+    results = {}
+    for function in functions:
+        results[function.__name__] = function(int_list)
+    return results
+
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
